9c5ad37ef830ce587a9362ec56e57eec6cbe3316
apostol.ionut <apostol_ionut@ymail.com>
2015-03-17 00:46:16 +0200

    asd

---
+++ WFW/Attributes/XamlTabIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Attributes
+{
+    public class XamlTabIndex : XamlAttribute
+    {
+        private const string WpfTabIndex = "TabIndex";
+
+        public XamlTabIndex(string line, XmlDocument xmlDocument)
+            :base(line,xmlDocument)
+        {
+            var value = line.Trim().Replace("this.TabIndex = ", String.Empty)
+                .Replace(";", String.Empty).Trim();
+
+            if(Validate(value))
+            {
+                XmlAttribute = xmlDocument.CreateAttribute(WpfTabIndex);
+                XmlAttribute.Value = value;
+                Processed = true;
+            }
+            else
+            {
+                Processed = false;
+            }
+        }
+    }
+}
