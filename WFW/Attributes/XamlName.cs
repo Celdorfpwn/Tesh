a3b66e349bbf1d09859581ec89ccb2b591b06470
iapostol <ionut.apostol@softvision.ro>
2015-03-17 10:51:37 +0200

    XamlName.cs

---
+++ WFW/Attributes/XamlName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Attributes
+{
+    public class XamlName : XamlAttribute
+    {
+        private const string WpfName = "Name";
+
+        public XamlName(string line, XmlDocument xmlDocument)
+            :base(line,xmlDocument)
+        {
+            Wpf = WpfName;
+            var value = line.Trim().Replace("this.Name = ", String.Empty)
+                .Replace("\"", String.Empty)
+                .Replace(";", String.Empty)
+                .Trim();
+
+            Validate(value, xmlDocument);
+        }
+    }
+}
