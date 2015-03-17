0ee412f3663efc3844a78773af67056861c229a4
iapostol <ionut.apostol@softvision.ro>
2015-03-17 18:46:32 +0200

    Controls

---
+++ WFW/XamlControls/XamlControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XamlControls
+{
+    public abstract class XamlControl
+    {
+
+        public bool Processed { get;private set; }
+
+        public XmlNode XmlNode { get;set; }
+
+        public XamlControl(IEnumerable<string> lines,XmlDocument document)
+        {
+
+        }
+    }
+}
